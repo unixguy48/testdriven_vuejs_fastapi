7842dddcf6858f1f1bb961ac59b2e99e6b44c869
unixguy48 <unixguy48@hotmail.com>
2022-12-28 13:47:10 -0600

    Vue Setup
    Configured Vue Frontend Service and validated communication with Backend Service.

---
+++ fastapi-vue/services/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Ping-Test',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      axios.get('/')
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      },
+    },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
