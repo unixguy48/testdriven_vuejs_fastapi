9ac2ba610e0968a19df2e74d0f655204f2ef747f
unixguy48 <unixguy48@hotmail.com>
2022-12-29 15:16:52 -0600

    CRUD Actions
    CRUD Actions Backend Implementation

--- fastapi-vue/services/backend/src/main.py
+++ fastapi-vue/services/backend/src/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from tortoise import Tortoise
 
 from src.database.register import register_tortoise
 from src.database.config import TORTOISE_ORM
+
+# enable schemas to read relationship between models
+Tortoise.init_models(
+    ["src.database.models"],
+    "models"
+)
 
 app = FastAPI()
 
